932dd672f1a0c61d9f56ef2a184bdfebe7ec2110
Sebastian madero <rockmadero@gmail.com>
2023-05-31 16:55:20 -0500

    primera pagina layout

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import '../styles/global.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Descubrir y compartir AI Prompts',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+}
