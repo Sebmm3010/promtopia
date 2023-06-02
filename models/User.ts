bd7a9bba9fc3f92454844fcf08e2370a2d335007
Sebastian madero <rockmadero@gmail.com>
2023-06-02 12:02:26 -0500

    modelo para usuarios

---
+++ models/User.ts
@@ -0,0 +1,25 @@
+import { IUser } from "@interfaces";
+import mongoose, { Schema, model, Model } from "mongoose";
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      validate: {
+        validator: function (userName: string) {
+          return userName.length >= 3;
+        },
+        message: "El nombre debe tener al menos 3 caracteres.",
+      },
+    },
+    email: { type: String, unique: true, require: true },
+    password: { type: String, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User:Model<IUser>=mongoose.models.User || model("User", userSchema);
+export default User;
+
