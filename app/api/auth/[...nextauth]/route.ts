f8a207701efe7b1d469da7019be3b945b3fcf509
Sebastian madero <rockmadero@gmail.com>
2023-06-02 13:20:38 -0500

    sign in route next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,13 @@
+import { dbConnection } from "@data";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { User } from "@models";
+
+interface user {
+  email: string;
+  name: string;
+  image?: string;
+}
 
 const handler = NextAuth({
   providers: [
@@ -10,15 +18,34 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user?.email });
+      session.user.id= sessionUser!._id.toString();
       return session;
     },
-    // async signIn({ profile }) {
-    //   try {
-        
-    //   } catch (error) {
-        
-    //   }
-    // },
+    async signIn({ profile }) {
+      const { email, name, image } = profile as user;
+      try {
+        await dbConnection();
+
+        //* Usuario existe?
+        const user = await User.findOne({
+          email,
+        });
+
+        // *Si el usuario no existe
+        if (!user) {
+          await User.create({
+            email: email,
+            name: name?.replace(" ", "").toLowerCase(),
+            image: image || "",
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
